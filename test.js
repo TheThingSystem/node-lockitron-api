88194189a5766cdbc4be2bd6e0949432489a4cea
Marshall Rose <mrose17@gmail.com>
2013-11-15 21:09:34 -0800

    ditto

--- test.js
+++ test.js
@@ -5,8 +5,8 @@ var Lockitron = require('./lockitron')
 lockitron.on('error', function(err) {
   console.log('lockitron error');
   console.error (err);
-}).setConfig('1ddd4eb12eaba16e38f79272fb07a48e7a6bb7f17ecbad4d4e14a1cd362dfc12',
-             'e1614db3a27c9c893968d4d6ce5f01118b720fca5fa77c5066bef08471a2a02f'
+}).setConfig('...'
+             '...'
 ).getDevices(function(err, results) {
   var f, i;
 
